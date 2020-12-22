47a6a35e1da14a1447a10186f9d93bce64312b5e
Jon Jackson <jon.jackson@perficient.com>
2020-12-22 15:33:12 -0500

    defined scrolljack component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React from "react";
 import colors from "./css-colors";
 import "./App.css";
-
-//https://www.selbekk.io/blog/2019/08/how-to-fade-in-content-as-it-scrolls-into-view/
-
-function FadeInSection(props) {
-  const [isVisible, setVisible] = React.useState(false);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-  }, []);
-  return (
-    <div
-      className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
-      ref={domRef}
-    >
-      {props.children}
-    </div>
-  );
-}
+import ScrollJack from "./components/ScrollJack";
 
 function App() {
   return (
     <div className="App">
-      <h1>All the CSS colors!</h1>
-
-      {/* {colors.map((color) => (
-        <FadeInSection key={color}>
-          <div className="box" style={{ backgroundColor: color }}>
-            <span>{color}</span>
-          </div>
-        </FadeInSection>
-      ))} */}
-      <FadeInSection>
-        <div className="jack">
-          <div className="img">image</div>
-          <div className="content">content</div>
-        </div>
-      </FadeInSection>
+      <ScrollJack />
     </div>
   );
 }
