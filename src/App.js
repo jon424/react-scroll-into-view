e61fb664586338b0d819319f969ffa50abb734ab
Jon <jonathanjackson424@gmail.com>
2021-01-01 23:15:11 -0500

    check out scrollmagic

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import colors from "./css-colors";
 import "./App.css";
 import ScrollJack from "./components/ScrollJack";
-
+//http://scrollmagic.io/examples/basic/section_wipes_natural.html
 function App() {
   return (
     <div className="App">
